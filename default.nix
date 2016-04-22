55f00e9832ecdad5832431bc140c6316ab01ccd0
Jean-Philippe Bernardy <jean-philippe.bernardy@tweag.io>
2016-04-22 20:09:25 +0200

    xe nix config

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ mkDerivation, base, lens, optparse-applicative, stdenv
+, taggy-lens, text, time, wreq
+}:
+mkDerivation {
+  pname = "xe";
+  version = "0.1";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    base lens optparse-applicative taggy-lens text time wreq
+  ];
+  description = "Get FX rates from xe.com";
+  license = stdenv.lib.licenses.unfree;
+}
